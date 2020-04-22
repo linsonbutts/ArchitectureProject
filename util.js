b0f5befae2e87cfc17d8ef1472768ad6d427da7e
Linson Butts <linson.butts@students.cau.edu>
2020-04-22 13:55:34 -0400

    Added collapse buttons and updated the javaScript to function alongside it

--- util.js
+++ util.js
@@ -1,22 +1,20 @@
 class Patient{
 
-    constructor(name, temp, src,conditions){
-        this.name = name; // patient's name
-        this.temp = temp;//temperature of patient
-        this.src= src;//image src
-        this.conditions =  conditions;
+    constructor(name, temp,conditions){
+        this._name = name; // patient's name
+        this._temp = temp;//temperature of patient
+
+        this._conditions =  conditions;
     }
 
     get name(){ return this._name;}
-    set name(name){this.name = name;}
-    get temp(){ return this.temp;}
-    set temp(temp){this.temp = temp;}
+    set name(name){this._name = name;}
+    get temp(){ return this._temp;}
+    set temp(temp){this._temp = temp;}
     
-    get src(){ return this.src;}
-    set src(src){this.src = src;}
     
-    get conditions(){return this.conditions;}
-    set conditions(conditions){this.conditions = conditions;}
+    get conditions(){return this._conditions;}
+    set conditions(conditions){this._conditions = conditions;}
 
 
 }
@@ -24,14 +22,15 @@ class Patient{
 function ready(){
     let patient1 = new Patient();
     patient1.name = 'Phony Pearson';
-    patient1.temp =  window.setInterval(function(){
-        let x = Math.random * 100 - 10;
-        if(x<87){
-            alert(patient1.name+"'s temperature is very high");
-            return x;
-        }
-        return x;
-    },5000);
+    patient1.temp =  Math.random() *100 -10;
+    patient1.conditions = ('Filler','Filler','Filler');
+    
+    let content = document.getElementsByClassName('collapse');
+
+    for(i =0; i<content.length; i++){
+        content[i].append(patient1.name,patient1.temp,patient1.conditions);
+    }
+
 }
 
 if (document.readyState !== "loading") {
