6e6851b2521706250d3e305f66b6a14f388554f6
Linson Butts <linson.butts@students.cau.edu>
2020-04-25 15:24:27 -0400

    Page fully operational

--- util.js
+++ util.js
@@ -22,14 +22,29 @@ class Patient{
 function ready(){
     let patient1 = new Patient();
     patient1.name = 'Phony Pearson';
-    patient1.temp =  Math.random() *100 -10;
+    patient1.temp =  Math.floor(Math.random() * (110 - 35 +1)+35);
     patient1.conditions = ('Filler','Filler','Filler');
+    if(patient1.temp >85 || patient1 < 50){
+        alert('Check '+patient1.name+"'s temperature it is abnormal")
+    }
+    
     
     let content = document.getElementsByClassName('collapse');
 
     for(i =0; i<content.length; i++){
-        content[i].append(patient1.name,patient1.temp,patient1.conditions);
+        content[i].append('Patient Name: '+patient1.name+', Patient Temperature:'+Math.round(patient1.temp)+', Patients Conditions:'+patient1.conditions);
     }
+    window.setInterval(function(){
+        alert('Temperatures have been updated');
+
+        patient1.temp =  Math.floor(Math.random() * (110 - 35 +1)+35);
+        if(patient1.temp >85 || patient1 < 50){
+            alert('Check '+patient1.name+"'s temperature it is abnormal");
+        }
+        for(i =0; i<content.length; i++){
+            content[i].innerHTML = (' Patient Name:'+patient1.name+', Patient Temperature:'+Math.round(patient1.temp)+', Patients Conditions:'+patient1.conditions);
+        }
+        },10000);
 
 }
 
